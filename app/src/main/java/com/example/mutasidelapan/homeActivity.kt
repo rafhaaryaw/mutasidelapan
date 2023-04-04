f04280ffbc481cc7c4216d15aa0128283bcad7ab
RAFA <wicaksonorafha@gmail.com>
2023-04-04 14:03:03 +0700

    home update

--- app/src/main/java/com/example/mutasidelapan/homeActivity.kt
+++ app/src/main/java/com/example/mutasidelapan/homeActivity.kt
@@ -2,10 +2,17 @@ package com.example.mutasidelapan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class homeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide()
+
+    //supportActionBar?.hide()
+
     }
 }

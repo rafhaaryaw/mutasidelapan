7bff4e9d3dd884add54e81b88500f3b15baa78af
RAFA <wicaksonorafha@gmail.com>
2023-04-14 00:02:47 +0700

    layout input data update

---
+++ app/src/main/java/com/example/mutasidelapan/ListDataAdapter.java
@@ -0,0 +1,6 @@
+package com.example.mutasidelapan;
+
+import java.util.ArrayList;
+
+public class ListDataAdapter{
+}
--- app/src/main/java/com/example/mutasidelapan/homeActivity.kt
+++ app/src/main/java/com/example/mutasidelapan/homeActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mutasidelapan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.annotation.StringRes
+import androidx.cardview.widget.CardView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.mutasidelapan.databinding.ActivityHomeBinding
 import com.google.android.material.tabs.TabLayout
@@ -11,7 +14,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class homeActivity : AppCompatActivity() {
 
-
+    private lateinit var image: ImageView
 
     private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +22,30 @@ class homeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    supportActionBar?.hide()
+        val card1: CardView = findViewById(R.id.card_profile_diri)
 
+        card1.setOnClickListener {
+            image = findViewById(R.id.iv_profile_diri)
+            uploadimage(image)
+        }
+
+        supportActionBar?.hide()
+
+    }
+
+    private fun uploadimage(image: ImageView?) {
+        val intent = Intent()
+
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        startActivityForResult(intent, 1)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            image.setImageURI(data?.data)
+        }
     }
 }
